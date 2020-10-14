613f5fe48eeacdd622590d87ed6f38a9ede12be7
Rafa Prieto <rapribu@gmail.com>
2020-10-14 18:20:20 +0200

    Serialize cart state through router events

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -3,9 +3,25 @@ import { Container } from './Container'
 import Footer from './Footer'
 import { useSelector } from 'react-redux'
 import Loader from './Loader'
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
 
 const Layout = ({ children }) => {
   const loadState = useSelector((state) => state.loading)
+  const router = useRouter()
+  const store = useSelector((state) => state)
+  useEffect(() => {
+    const serializeState = () => {
+      console.log('serialize state')
+
+      window.localStorage.setItem('store', JSON.stringify(store))
+    }
+    router.events.on('routeChangeStart', serializeState)
+
+    return () => {
+      router.events.off('routeChangeStart', serializeState)
+    }
+  }, [store])
   return (
     <>
       <Header />
