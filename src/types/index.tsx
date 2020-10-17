b885125b35b2507acc781c6369fea8eac110c1b3
Rafa Prieto <rapribu@gmail.com>
2020-10-17 13:49:24 +0200

    Add types description file

--- src/types/index.tsx
+++ src/types/index.tsx
@@ -1,8 +1,23 @@
-export interface cartStateType {
+import { Action, ActionCreator, Dispatch } from 'redux'
+
+export interface CartStateType {
   isCartOpen: boolean
   setIsCartOpen: React.Dispatch<React.SetStateAction<boolean>>
 }
-export interface responsiveMenuState {
+export interface ResponsiveMenuState {
   isResMenuOpen: boolean
   setIsResMenuOpen: React.Dispatch<React.SetStateAction<boolean>>
+}
+
+export interface CartAction extends Action<string> {
+  id: number
+}
+
+export interface Product {
+  id: number
+  description: string
+  title: string
+  price: number
+  image: string
+  category: string
 }
